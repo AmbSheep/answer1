cc9cd628d32c6348ec2f7e583058cc03c36507a5
AmbSheep <creamghost@mail.ru>
2020-07-17 14:18:52 +0300

    changed to own function

--- Squarefield.py
+++ Squarefield.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 import sys
-import math
-l = int(sys.argv[1])
-w = int(sys.argv[2])
-gcd = math.gcd(l, w)
-print(gcd, 'x', gcd)
+def gcd(a, b):
+    while a != b:
+        if a>b:
+            a = a-b
+        else:
+            b = b-a
+    return a
+
+if __name__=='__main__':
+    print(gcd(int(sys.argv[1]), int(sys.argv[2])), 'x', gcd(int(sys.argv[1]), int(sys.argv[2])))
 
